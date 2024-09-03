dcdb71737f8c6ed89d95ce1f94d01dfa4c756dd4
Belial_Alu <127793189+BelialALu@users.noreply.github.com>
2024-09-03 16:59:52 +0300

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,11 +1,17 @@
 function finishQuiz(subject) {
     const userAnswers = {};
-    const questions = document.querySelectorAll('.question input[type="text"]');
+    for (let i = 1; i <= 10; i++) {
+        const answer = document.getElementById(`question-${i}`).value;
+        userAnswers[i] = answer;
+    }
+    localStorage.setItem(`quizAnswers_${subject}`, JSON.stringify(userAnswers));
     
-    questions.forEach((input, index) => {
-        userAnswers[index + 1] = input.value; // Используйте индекс + 1 для соответствия номерам вопросов
-    });
-
-    localStorage.setItem(`quizAnswers_${subject}`, JSON.stringify(userAnswers)); // Сохраняем ответы
-    window.location.href = `results.html?subject=${subject}`; // Переходим на страницу результатов
+    // Перенаправление на соответствующую страницу результатов
+    const resultsPages = {
+        "math": "results_math.html",
+        "russian": "results_russian.html",
+        "informatics": "results_informatics.html"
+    };
+    
+    window.location.href = resultsPages[subject];
 }
