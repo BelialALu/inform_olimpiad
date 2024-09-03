ac84282d973023ecc0d2eca01e6af876099b477c
Belial_Alu <127793189+BelialALu@users.noreply.github.com>
2024-09-03 16:28:51 +0300

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,13 @@
 function finishQuiz(subject) {
     const userAnswers = {};
-    for (let i = 1; i <= 10; i++) {
+    let i = 1;
+
+    while (document.getElementById(`question-${i}`)) {
         const answer = document.getElementById(`question-${i}`).value;
         userAnswers[i] = answer;
+        i++;
     }
+
     localStorage.setItem(`quizAnswers_${subject}`, JSON.stringify(userAnswers)); // Сохраняем ответы
     window.location.href = `results.html?subject=${subject}`; // Переходим на страницу результатов
 }
